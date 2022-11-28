4a912bbf5162a7c87df1ad176ff72ace288a8ee4
EuMotta <Hyperxmotta@hotmail.com>
2022-11-28 13:02:37 -0300

    NextAuth + Mui

--- components/Header.js
+++ components/Header.js
@@ -5,16 +5,23 @@ import HeaderIcons from './HeaderIcons'
 import {
   FlagIcon,
   PlayIcon,
-  ShoppingCartIcon,
   UserGroupIcon
 } from '@heroicons/react/24/outline'
 import {
   HomeIcon
 } from '@heroicons/react/24/solid'
+import CottageIcon from '@mui/icons-material/Cottage';
+import AppsIcon from '@mui/icons-material/Apps';
+import OutlinedFlagTwoToneIcon from '@mui/icons-material/OutlinedFlagTwoTone';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import ChatIcon from '@mui/icons-material/Chat';
+import styled from 'styled-components'
+import { Avatar } from '@mui/material'
 
 export default function HeaderScreen() {
   return (
-    <div className='flex sticky top-0 bg-white p-2 lg:px-5 shadow-md items-center'>
+    <div className='flex sticky top-0 h-14 bg-white p-2 lg:px-5 shadow-md items-center'>
       <div className='flex items-center'>
         <Image
           src="https://links.papareact.com/5me"
@@ -28,20 +35,35 @@ export default function HeaderScreen() {
           <input
             type="text"
             placeholder='Pesquisar'
-            className='focus:text-white flex-shrink px-2 focus:shadow-md focus:shadow-slate-500 focus:bg-indigo-100 rounded-full focus:border-indigo-400 focus:outline-none  text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300  transition ease-in-out m-0 '
+            className='focus:text-white hidden md:inline-flex flex-shrink px-2 focus:shadow-md focus:shadow-slate-500 focus:bg-indigo-100 rounded-full focus:border-indigo-400 focus:outline-none  text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300  transition ease-in-out m-0 '
           />
-          <FcSearch className='relative text-2xl top-1  right-8' />
+          <FcSearch className='relative text-2xl top-1 hidden md:inline-flex  right-8' />
         </div>
       </div>
       <div className='flex gap-x-10 items-center'>
-        <div className='flex gap-x-10 '>
-          <HeaderIcons Icon={HomeIcon} active />
-          <HeaderIcons Icon={FlagIcon} />
-          <HeaderIcons Icon={PlayIcon} />
-          <HeaderIcons Icon={ShoppingCartIcon} />
-          <HeaderIcons Icon={UserGroupIcon } />
+        <div className='flex gap-x-5 '>
+          <HeaderIcons Icon={CottageIcon} active />
+          <HeaderIcons Icon={OutlinedFlagTwoToneIcon} />
+          <HeaderIcons Icon={PlayCircleOutlineIcon} />
+          <HeaderIcons Icon={ShoppingCartCheckoutIcon} />
+          <HeaderIcons Icon={UserGroupIcon} />
         </div>
       </div>
+      <Direita className='gap-3 justify-end'> 
+      
+          <p className='whitespace-nowrap text-xl  font-semibold pr-3'>José Antonio</p>
+          <AppsIcon className='icon xl:inline-flex hover:bg-slate-300' />
+          <ChatIcon className='icon xl:inline-flex hover:bg-slate-300'/>
+        <Avatar className='cursor-pointer' />
+        
+      </Direita>
     </div>
   )
 }
+
+const Direita = styled.div`
+display:flex;
+width:100%;
+height:80%;
+align-items:center;
+`;
--- components/HeaderIcons.js
+++ components/HeaderIcons.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function HeaderIcons({Icon,active}) {
   return (
-    <div className='cursor-pointer flex items-center md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-md active:border-b-2 active:border-red-500 group'>
+    <div className='cursor-pointer flex items-center md:px-5 sm:h-10 md:hover:bg-gray-100 rounded-md active:border-b-2 active:border-red-500 group'>
         <Icon className={`h-8 group-hover:text-red-600 sm:h-6 text-center text-slate-700 mx-auto
         ${active && 'text-red-600'}
         `}/>
