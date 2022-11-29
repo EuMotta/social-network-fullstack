24f0a0768c2728fee0570d5bad9f72fdf50e83b3
EuMotta <Hyperxmotta@hotmail.com>
2022-11-29 20:28:47 -0300

    auth correction

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,14 @@
 import NextAuth from "next-auth"
-import Providers from 'next-auth/providers'
+import Providers from "next-auth/react"
 
 export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    // Configure one or more authentication providers
+    providers: [
+        Providers.Facebook({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+        // ...add more providers here
+    ],
 }
-
 export default NextAuth(authOptions)
