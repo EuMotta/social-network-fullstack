7385f9bd4b98f8b19bb831b20264f6f7330a709c
EuMotta <Hyperxmotta@hotmail.com>
2022-12-03 18:37:59 -0300

    reCreation

--- components/Layout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Layout() {
-  return (
-    <div className='text-8xl'>Layout</div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,14 @@
-import { getSession } from 'next-auth/react'
 import Head from 'next/head'
-import HeaderScreen from '../components/Header'
-import LoginScreen from '../components/Login'
-import Sidebar from '../components/Sidebar'
+import Image from 'next/image'
 
-export default function Home({session}) {
-  // if(!session) return<LoginScreen/>
+export default function Home() {
   return (
-    <div className='h-screen bg-slate-100 overflow-hidden'>
+    <div>
       <Head>
-        <title>FateCanos</title>
       </Head>
-      {/* Login */}
-      <HeaderScreen/>
-        {/* Chat */}
-      <main className='flex'>
-         <Sidebar/>
-
-        {/* children */}
-        {/* widget */}
-      </main>
-
-      <footer>
-        {/* ScrollUp */}
-        {/*  */}
-      </footer>
+      <div className='text-5xl'>
+        aa
+      </div>
     </div>
   )
-}
-
-export async function getServerSideProps(context){
-  const session = await getSession(context)
-  return{
-    props:{
-      session,
-    }
-  }
 }
