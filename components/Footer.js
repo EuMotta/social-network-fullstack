7b503c752992740290a56ac47bca192f6b00ac3c
EuMotta <hyperxmotta@hotmail.com>
2022-12-10 14:50:48 -0300

    teste de conexão

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
                     <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <a href="# " class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
                 </li>
                 <li>
                     <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
